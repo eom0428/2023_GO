c0159914d1e568d1668de6cb6df4e25093762f27
eom <202344011a@email.com>
2023-09-25 15:00:04 +0900

    week05 v0.6

--- week05/main.go
+++ week05/main.go
@@ -3,12 +3,14 @@ package main
 import (
 	"bufio"
 	"fmt"
+	"log"
 	"os"
 )
 
 func main() {
 	fmt.Print("input Score : ")
 	reader := bufio.NewReader(os.Stdin)
-	inputNumber, _ := reader.ReadString('\n') //1 variable but reader.ReadString returns 2 values,  err declared and not used, option 1
+	inputNumber, err := reader.ReadString('\n') // option 2
+	log.Fatal(err)
 	fmt.Println(inputNumber)
 }
