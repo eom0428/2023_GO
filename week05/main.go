2dbaff8e25701b8dacf61e352b47e394124b275f
eom <202344011a@email.com>
2023-09-25 15:35:29 +0900

    week05 v1.2

--- week05/main.go
+++ week05/main.go
@@ -1,32 +1,12 @@
 package main
 
 import (
-	"bufio"
 	"fmt"
-	"log"
-	"os"
-	"strconv"
-	"strings"
+	"math/rand"
 )
 
 func main() {
-	fmt.Print("input Score : ")
-	reader := bufio.NewReader(os.Stdin)
-	inputNumber, err := reader.ReadString('\n') // option 2
-	if err != nil {
-		log.Fatal(err)
-	}
-	inputNumberString = strings.TrimSpace(inputNumberString) // remove space bar
-	inputNumber, err := strconv.ParseFloat(inputNumberString, 32)
 
-	if err != nil {
-		log.Fatal(err)
-	}
-	var grade string
-	if inputNumber >= 90 {
-		grade = "A grade!"
-	} else {
-		grade = "BCDE grade~"
-	}
-	fmt.Println(" you got" + grade)
+	dice := rand.Intn(6) + 1
+	fmt.Println(dice)
 }
