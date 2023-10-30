79f0f95913374a490162232375a8392618e61ddb
eom <202344011a@email.com>
2023-10-30 14:47:26 +0900

    week10 v2.1

--- week10/main.go
+++ week10/main.go
@@ -1,8 +1,14 @@
 package main
 
-import "week10/src/greeting"
+import (
+	"fmt"
+	"week10/src/greeting"
+	"week10/src/mymath"
+)
 
 func main() {
+	fmt.Println(mymath.MyPower(2, 9))
+	fmt.Println(mymath.MyAbs(mymath.MyAbs(-99)))
 	greeting.Hello()
 	greeting.Hi()
 }
