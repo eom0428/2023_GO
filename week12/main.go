648dd6fc38fa457994a9d7b44d079dee97bc41b1
eom <202344011a@email.com>
2023-11-13 14:23:51 +0900

    week12 v.2.9

--- week12/main.go
+++ week12/main.go
@@ -6,9 +6,13 @@ func main() {
 	//var s []int
 	//s = make([]int, 5)
 
-	//s := make([]int, 5) 단축 연산자
+	//s := make([]int, 5) 단축 연산자 make 함수 이용
 
-	s := []int{0, 0, 0, 0, 0}
+	s := []int{0, 0, 0, 0, 0} //슬라이스 리터럴을 이용
+
+	s[4] = 99
+	s[2] = 91
+
 	for _, value := range s {
 		fmt.Println(value)
 	}
