f4270ef4c908647ab1558ebed276fff5f0f17313
eom <202344011a@email.com>
2023-11-13 15:00:23 +0900

    week12 v3.3

--- week12/main.go
+++ week12/main.go
@@ -3,25 +3,13 @@ package main
 import "fmt"
 
 func main() {
+	a := []string{"a", "b", "c", "d"}
+	as := a[0:2]
+	as[1] = "z"
+	fmt.Println(a)
+	fmt.Println(as)
 
-	s := []int{0, 0, 0, 0, 0}
-
-	s[4] = 99
-	s[2] = 91
-
-	for _, value := range s {
-		fmt.Println(value)
-	}
-
-	copyS := s[1:4]
-	for _, value := range copyS {
-		fmt.Println(value)
-	}
-
-	test := [3]string{"hamin", "bambi", "noha"} //배욜 리터럴을 이용해서 test 배열 생성
-	testS := test[0:2]
-	testS2 := test[1:]
-	testS2[0] = "yejun"
-	fmt.Println(testS2)
-	fmt.Println(testS, len(testS))
+	b := [4]int{4, 3, 2, 1}
+	bs := b[1:3]
+	fmt.Println(bs)
 }
