0704bcf5cc6d0bec96f1f55d9d9f099a9e482e17
eom <202344011a@email.com>
2023-11-20 14:48:15 +0900

    week13 v4.0

--- week13/main.go
+++ week13/main.go
@@ -5,9 +5,10 @@ import "fmt"
 func main() {
 	var a []string
 	var b []bool
-	a = make([]string, 4, 5)
+	// a = make([]string, 4, 5)
 
-	fmt.Println(b, len(b), cap(b))
+	b = append(b, true)
 	fmt.Printf("%#v %#v\n", a, b)
+	fmt.Println(b, len(b), cap(b))
 	fmt.Println(a, len(a), cap(a))
 }
